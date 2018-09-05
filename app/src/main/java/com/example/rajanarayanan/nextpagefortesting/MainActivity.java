2c735f75b7bad716f387bfebae80aa4d0af202a8
rajanarayanan <raja.narayanan@widas.in>
2018-09-05 17:19:21 +0530

    first commit

---
+++ app/src/main/java/com/example/rajanarayanan/nextpagefortesting/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.rajanarayanan.nextpagefortesting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void nextActivity(View view){
+        Toast.makeText(this, "Next a", Toast.LENGTH_SHORT).show();
+        Intent intent=new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+}
